9b1b49c3a4834a03b0c2ef953e23345412eb0334
Tiag0Sants <mbetani.betania.10@gmail.com>
2024-04-28 15:48:06 -0300

    intent do Login -> Home e Home -> Suporte

--- app/src/main/java/com/example/prototipo2/Home.java
+++ app/src/main/java/com/example/prototipo2/Home.java
@@ -2,13 +2,34 @@ package com.example.prototipo2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class Home extends AppCompatActivity {
+public class Home extends AppCompatActivity implements View.OnClickListener {
+
+    Button atendimento;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        atendimento = (Button) findViewById(R.id.atendimento);
+        atendimento.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.atendimento) {
+            //se clicou no botão atendimento ao cliente
+            Intent tela = new Intent(this, suporte.class);
+            startActivity(tela);
+        }
+
+
     }
 }
+
