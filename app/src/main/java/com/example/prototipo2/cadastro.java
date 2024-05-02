dc8abdd9a9d201115863d78fd519aad25ab66d49
kihus <wintergamesk@gmail.com>
2024-05-02 18:23:36 -0300

    alteracoes dos ID dos editBox do activity_cadastro.xml, so foi colocado o "cadastro_" antes do nome do ID

--- app/src/main/java/com/example/prototipo2/cadastro.java
+++ app/src/main/java/com/example/prototipo2/cadastro.java
@@ -6,15 +6,27 @@ import android.content.Intent;
 import android.view.View;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 import android.os.Bundle;
 
 
 public class cadastro extends AppCompatActivity {
 
+    EditText nome, email, senha;
+    Button cadastrar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro);
+
+        nome = findViewById(R.id.cadastro_nome);
+        email = findViewById(R.id.cadastro_email);
+        senha = findViewById(R.id.cadastro_senha);
+
+
     }
 }
