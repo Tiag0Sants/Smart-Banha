b471e6d0b152c98524248cd23ef1dd7ec96b7c93
kihus <wintergamesk@gmail.com>
2024-05-02 18:11:33 -0300

    nem me lembro oq fiz

--- app/src/main/java/com/example/prototipo2/FormLogin.java
+++ app/src/main/java/com/example/prototipo2/FormLogin.java
@@ -1,10 +1,18 @@
 package com.example.prototipo2;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import java.util.Objects;
 import android.widget.Button;
 
 public class FormLogin extends AppCompatActivity implements View.OnClickListener {
--- app/src/main/java/com/example/prototipo2/cadastro.java
+++ app/src/main/java/com/example/prototipo2/cadastro.java
@@ -2,7 +2,13 @@ package com.example.prototipo2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.view.View;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import android.widget.Toast;
 import android.os.Bundle;
+
 
 public class cadastro extends AppCompatActivity {
 
