baf3ba8701109be945af11e035acf192ea60d733
kihus <wintergamesk@gmail.com>
2024-05-07 20:35:36 -0300

    firebase é otaku, petista e nao lhe tanko

--- app/src/main/java/com/example/prototipo2/FormLogin.java
+++ app/src/main/java/com/example/prototipo2/FormLogin.java
@@ -29,6 +29,69 @@ public class FormLogin extends AppCompatActivity {
         login_email = findViewById(R.id.login_email);
         login_senha = findViewById(R.id.login_senha);
         botao_login = findViewById(R.id.botao_login);
+        botao_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!validateUsername() | !validatePassword()) {
+                } else {
+                    checkUser();
+                }
+            }
+        });
 
     }
-}
+    public Boolean validateUsername() {
+        String val = login_email.getText().toString();
+        if (val.isEmpty()) {
+            login_email.setError("Username cannot be empty");
+            return false;
+        } else {
+            login_email.setError(null);
+            return true;
+        }
+    }
+    public Boolean validatePassword(){
+        String val = login_senha.getText().toString();
+        if (val.isEmpty()) {
+            login_senha.setError("Password cannot be empty");
+            return false;
+        } else {
+            login_senha.setError(null);
+            return true;
+        }
+    }
+    public void checkUser(){
+        String userEmail = login_email.getText().toString().trim();
+        String userSenha = login_senha.getText().toString().trim();
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("users");
+        Query checkUserDatabase = reference.orderByChild("email").equalTo(userEmail);
+        checkUserDatabase.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if (snapshot.exists()){
+                    login_email.setError(null);
+                    String passwordFromDB = snapshot.child(userEmail).child("senha").getValue(String.class);
+                    if (passwordFromDB.equals(userSenha)) {
+                        login_email.setError(null);
+                        String nameFromDB = snapshot.child(userEmail).child("nome").getValue(String.class);
+                        String emailFromDB = snapshot.child(userEmail).child("email").getValue(String.class);
+                        Intent intent = new Intent(FormLogin.this, MainActivity.class);
+                        intent.putExtra("nome", nameFromDB);
+                        intent.putExtra("email", emailFromDB);
+                        intent.putExtra("password", passwordFromDB);
+                        startActivity(intent);
+                    } else {
+                        login_senha.setError("Invalid Credentials");
+                        login_senha.requestFocus();
+                    }
+                } else {
+                    login_email.setError("User does not exist");
+                    login_email.requestFocus();
+                }
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+            }
+        });
+    }
+}   
